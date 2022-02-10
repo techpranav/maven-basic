372fab59a4a12172b88e91fe7b92f22cb9fb0fff
techpranav <pranavshirsul896@gmail.com>
2022-02-10 16:08:32 +0530

    Update Java

--- src/main/java/com/acme/basic/HelloWorld.java
+++ src/main/java/com/acme/basic/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
   void sayHello() {
     System.out.println("Hello World!");
+    System.out.println("Hello World!");
+    System.out.println("Hello World!");
   }
 
   void notCovered() {
